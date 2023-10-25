52faa38ae3613bb2b72e31471f32dd342b1b6073
BooManLag <74959572+BooManLag@users.noreply.github.com>
2023-10-25 23:25:33 +0800

    Timer Added

--- lib/screens/screen_game.dart
+++ lib/screens/screen_game.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:linear_timer/linear_timer.dart'; // <- Import the linear_timer package
 import '../bloc/bloc_game.dart';
 import '../bloc/bloc_game_event.dart';
 import '../bloc/bloc_game_state.dart';
@@ -15,6 +16,8 @@ class _GameScreenState extends State<GameScreen> {
   bool isButtonDisabled = false;
   String lastKnownQuestion = '';
   List<String> lastKnownOptions = [];
+
+  ValueKey<int> timerKey = const ValueKey<int>(0); // Added key for LinearTimer
 
   Future<void> handleAnswer(
       GameBloc gameBloc, String option, GameState state) async {
@@ -45,7 +48,7 @@ class _GameScreenState extends State<GameScreen> {
     // Add the AnswerQuestion event with the given option
     gameBloc.add(AnswerQuestion(answer: option));
 
-    await Future.delayed(Duration(seconds: 2));
+    await Future.delayed(const Duration(seconds: 2));
 
     setState(() {
       isButtonDisabled = false;
@@ -81,6 +84,9 @@ class _GameScreenState extends State<GameScreen> {
               state.questions[state.currentQuestionIndex]['question'];
           lastKnownOptions = List<String>.from(
               state.questions[state.currentQuestionIndex]['options'] ?? []);
+
+          timerKey = ValueKey<int>(state
+              .currentQuestionIndex); // Reset the timer for every new question
         }
 
         return Scaffold(
@@ -91,57 +97,83 @@ class _GameScreenState extends State<GameScreen> {
                 fit: BoxFit.cover,
               ),
             ),
-            child: Center(
-              child: Container(
-                padding: const EdgeInsets.all(20),
-                decoration: BoxDecoration(
-                  color: Colors.white.withOpacity(0.9),
-                  borderRadius: BorderRadius.circular(12),
+            child: Column(
+              children: [
+                // LinearTimer integration
+                Padding(
+                  padding: const EdgeInsets.all(20.0),
+                  child: LinearTimer(
+                    key: timerKey, // Attach the key here
+                    duration: const Duration(seconds: 10),
+                    forward: false,
+                    onTimerEnd: () {
+                      // When the timer finishes, move to the next question
+                      context.read<GameBloc>().add(NextQuestion());
+                    },
+                    color: Colors.blue, // Optional: Set your preferred color
+                    backgroundColor: Colors
+                        .white, // Optional: Set your preferred background color
+                    minHeight: 5.0, // Optional: Adjust based on your preference
+                  ),
                 ),
-                child: Column(
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    Text(
-                      lastKnownQuestion,
-                      style: const TextStyle(fontSize: 24),
-                    ),
-                    const SizedBox(height: 20),
-                    ...lastKnownOptions
-                        .map(
-                          (option) => Padding(
-                            padding:
-                                const EdgeInsets.only(bottom: 8.0, top: 8.0),
-                            child: Material(
-                              elevation: 2.0,
-                              borderRadius: BorderRadius.circular(12),
-                              child: InkWell(
-                                onTap: isButtonDisabled
-                                    ? null
-                                    : () async {
-                                        await handleAnswer(
-                                            context.read<GameBloc>(),
-                                            option,
-                                            state);
-                                      },
-                                child: Container(
-                                  padding: const EdgeInsets.all(16.0),
-                                  decoration: BoxDecoration(
+                // Rest of your game UI
+                Expanded(
+                  child: Center(
+                    child: Container(
+                      padding: const EdgeInsets.all(20),
+                      decoration: BoxDecoration(
+                        color: Colors.white.withOpacity(0.9),
+                        borderRadius: BorderRadius.circular(12),
+                      ),
+                      child: Column(
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          Text(
+                            lastKnownQuestion,
+                            style: const TextStyle(fontSize: 24),
+                          ),
+                          const SizedBox(height: 20),
+                          ...lastKnownOptions
+                              .map(
+                                (option) => Padding(
+                                  padding: const EdgeInsets.only(
+                                      bottom: 8.0, top: 8.0),
+                                  child: Material(
+                                    elevation: 2.0,
                                     borderRadius: BorderRadius.circular(12),
-                                    color: Colors.white,
-                                  ),
-                                  child: Text(
-                                    option,
-                                    style: const TextStyle(color: Colors.black),
+                                    child: InkWell(
+                                      onTap: isButtonDisabled
+                                          ? null
+                                          : () async {
+                                              await handleAnswer(
+                                                  context.read<GameBloc>(),
+                                                  option,
+                                                  state);
+                                            },
+                                      child: Container(
+                                        padding: const EdgeInsets.all(16.0),
+                                        decoration: BoxDecoration(
+                                          borderRadius:
+                                              BorderRadius.circular(12),
+                                          color: Colors.white,
+                                        ),
+                                        child: Text(
+                                          option,
+                                          style: const TextStyle(
+                                              color: Colors.black),
+                                        ),
+                                      ),
+                                    ),
                                   ),
                                 ),
-                              ),
-                            ),
-                          ),
-                        )
-                        .toList(),
-                  ],
+                              )
+                              .toList(),
+                        ],
+                      ),
+                    ),
+                  ),
                 ),
-              ),
+              ],
             ),
           ),
         );
